3d6eb4d7738e5a2e10ce379af337c879eaff2a89
devon <devonjayhumphries@gmail.com>
2019-03-25 18:45:27 -0400

    added styles to menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,71 +1,73 @@
 // JavaScript Document
+
 window.onload = init();
 
 function init() {
 
-  window.addEventListener('scroll', function(e) {
-    var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-      shrinkOn = 300,
-      header = document.querySelector("header");
-    if (distanceY > shrinkOn) {
-      classie.add(header, "smaller");
-    } else {
-      if (classie.has(header, "smaller")) {
-        classie.remove(header, "smaller");
-      }
-    }
-  });
+    window.addEventListener('scroll', function (e) {
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 300,
+            header = document.querySelector("header");
+        if (distanceY > shrinkOn) {
+            classie.add(header, "smaller");
+        } else {
+            if (classie.has(header, "smaller")) {
+                classie.remove(header, "smaller");
+            }
+        }
+    });
 
 
-  $.ajax({
-    method: 'GET',
-    url: 'assets/data/menu.json',
-    dataType: 'json',
-    success: function(data) {
+    $.ajax({
+        method: 'GET',
+        url: 'assets/data/menu.json',
+        dataType: 'json',
+        success: function (data) {
 
-      var menu = menuBuilder(data.menu);
+            var menu = menuBuilder(data.menu);
 
-    };
-    error: function() {
+            $('nav').append(menu);
 
-      console.log('all is not good');
+        },
+        error: function () {
 
-    }
+            console.log('all is not good');
 
-  });
+        }
+    });
+
 }
-
 
 
 function menuBuilder(obj) {
 
-  var theMenu = '';
+    var theMenu = '';
 
-  if (obj.length > 0) {
+    if (obj.length > 0) {
 
-    theMenu = theMenu + '<ul>';
+        theMenu = theMenu + '<ul>';
 
-    obj.forEach(function(item) {
+        obj.forEach(function (item) {
 
-      theMenu = theMenu + '<li><a href="#">' + item.MenuName + '</a>';
+            theMenu = theMenu + '<li><a href="#">' + item.MenuName + '</a>';
 
-      if (item.menus.length > 0) {
+            if (item.Menus.length > 0) {
 
-        theMenu = theMenu + menuBuilder(item.Menus);
+                theMenu = theMenu + menuBuilder(item.Menus);
 
-      }
+            }
 
-      theMenu = theMenu + '</li>';
+            theMenu = theMenu + '</li>';
 
-    });
+        });
 
-    theMenu = theMenu + '</ul>';
+        theMenu = theMenu + '</ul>';
 
+    } else {
 
-  } else {
+        console.log('no data');
 
-    console.log('no data');
-  }
+    }
 
-  return theMenu;
+    return theMenu;
 }
