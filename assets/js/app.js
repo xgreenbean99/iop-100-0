2fc4747b85a664d4ed755d8734580fa8299b6419
devon <devonjayhumphries@gmail.com>
2019-03-25 18:02:28 -0400

    added sub menus

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,54 +1,71 @@
 // JavaScript Document
-
+window.onload = init();
 
 function init() {
 
-    window.addEventListener('scroll', function (e) {
-        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-            shrinkOn = 300,
-            header = document.querySelector("header");
-        if (distanceY > shrinkOn) {
-            classie.add(header, "smaller");
-        } else {
-            if (classie.has(header, "smaller")) {
-                classie.remove(header, "smaller");
-            }
-        }
-    });
+  window.addEventListener('scroll', function(e) {
+    var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+      shrinkOn = 300,
+      header = document.querySelector("header");
+    if (distanceY > shrinkOn) {
+      classie.add(header, "smaller");
+    } else {
+      if (classie.has(header, "smaller")) {
+        classie.remove(header, "smaller");
+      }
+    }
+  });
 
 
-    $.ajax({
-        method: 'GET',
-        url: 'assets/data/menu.json',
-        dataType: 'json',
-        success: function (data) {
+  $.ajax({
+    method: 'GET',
+    url: 'assets/data/menu.json',
+    dataType: 'json',
+    success: function(data) {
 
-            console.log('all good');
-            console.log(data.menu.length);
-            console.log(data.menu);
+      var menu = menuBuilder(data.menu);
 
-            if (data.menu.length > 0) {
+    };
+    error: function() {
 
-                data.menu.forEach(function (data) {
+      console.log('all is not good');
 
-                    console.log(data.MenuName);
-                    console.log(data.MenuLink);
+    }
 
-                    $('nav').append('<a href="' + data.MenuLink + '">' + data.MenuName + '</a>');
-
-                });
-            }
-
-        },
-        error: function () {
-            console.log('all is not good');
-        }
-    });
-
-
-
+  });
 }
 
 
 
-window.onload = init();
+function menuBuilder(obj) {
+
+  var theMenu = '';
+
+  if (obj.length > 0) {
+
+    theMenu = theMenu + '<ul>';
+
+    obj.forEach(function(item) {
+
+      theMenu = theMenu + '<li><a href="#">' + item.MenuName + '</a>';
+
+      if (item.menus.length > 0) {
+
+        theMenu = theMenu + menuBuilder(item.Menus);
+
+      }
+
+      theMenu = theMenu + '</li>';
+
+    });
+
+    theMenu = theMenu + '</ul>';
+
+
+  } else {
+
+    console.log('no data');
+  }
+
+  return theMenu;
+}
